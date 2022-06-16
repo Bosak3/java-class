28922d97531dd414c7ed231c667127c174f46529
Adam <bosak3@gmail.com>
2022-06-16 17:23:31 -0500

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/bosak3/helloworld/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *@author Adam Bosak
+ *email: bosak3@gmail.com
+ *date: 06/16/22
+ *purpose: My first Hello World program in an IDE
+ */
+
+// com.bosak3.helloworld
+package com.bosak3.helloworld; 
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Adam!");
+    }
+}
